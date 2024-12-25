0ade5e3633b79484f07bddb854032a83d88fca21
Mikhail <boex2013@mail.ru>
2024-12-25 18:55:47 +0800

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['89.104.71.159'],
+        domains: ['192.168.0.15'],
     },
     env: {
         API_URL: 'https://aromosa.ru/api'
     },
-    experimental: {
-        appDir: false,
-      },
 };
 
 export default nextConfig;
