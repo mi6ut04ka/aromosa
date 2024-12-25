36bb385e57ee524b38e10839e4bc22369ee21ae4
Mikhail <boex2013@mail.ru>
2024-12-25 15:45:01 +0800

    add Suspense

--- app/cart/layout.js
+++ app/cart/layout.js
@@ -1,19 +1,21 @@
-'use client'
+import { Suspense } from 'react';
 import NextBreadcrumbs from "../components/ui/NextBreadcrumbs";
 
-export default function layout({children}) {
-    return (
-        <>
+export default function Layout({ children }) {
+  return (
+    <>
+      <Suspense fallback={<div>Загрузка...</div>}>
         <NextBreadcrumbs
-                getDefaultTextGenerator={(path) => {
-                  const customNames = {
-                    'cart': 'Корзина',
-                    'checkout': 'Оформление заказа'
-                  };
-                  return customNames[path] || path.replace(/-/g, ' ').charAt(0).toUpperCase() + path.slice(1);
-                }}
-                />
-        {children}
-        </>
-    )
-};
+          getDefaultTextGenerator={(path) => {
+            const customNames = {
+              cart: 'Корзина',
+              checkout: 'Оформление заказа',
+            };
+            return customNames[path] || path.replace(/-/g, ' ').charAt(0).toUpperCase() + path.slice(1);
+          }}
+        />
+      </Suspense>
+      {children}
+    </>
+  );
+}
